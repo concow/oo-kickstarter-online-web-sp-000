18d11857931d1596cc4ec0405ca5aa91aa92a6ae
Connor Cowan <ccowan003@comcast.net>
2019-07-08 06:35:18 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,6 +9,6 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    project.backer << self
+    project.backers << self               #back_project adds the Backer (self) to the @backers array in Project class
   end
 end
